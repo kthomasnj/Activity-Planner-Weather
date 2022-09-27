74904a651fea7a8d41816bfd55367b0f69dc106e
hasnafar <hasnafarrukh@gmail.com>
2022-09-27 19:58:00 -0400

    Add functionality to activities.js

--- models/Activities.js
+++ models/Activities.js
@@ -1,2 +1,40 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Activities extends Model {}
+
+Activities.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      weather: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      temperatureRange: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'activties'
+    }
+  );
+  
+  module.exports = Activities;
