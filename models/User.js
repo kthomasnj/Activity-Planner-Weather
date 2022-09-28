2c56941a817e064cfcf66987c24ccaca2b10145b
devin <devin@devins-MacBook-Pro.local>
2022-09-27 21:15:29 -0400

    added functionality to User model

--- models/User.js
+++ models/User.js
@@ -2,4 +2,44 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
+  
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+      // only allows alphanumeric characters
+          isAlphanumeric: true,
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      // password must be 8 or more characters
+        validate: {
+          len: [8],
+        },
+      },
+    },
+  );
 module.exports = User;
