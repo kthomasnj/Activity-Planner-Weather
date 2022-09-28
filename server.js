fe69c3766f5fbeb82ded08072ace58895fd63ed0
hasnafar <hasnafarrukh@gmail.com>
2022-09-27 20:13:53 -0400

    connect back end to front end

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+ 
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +10,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
 
