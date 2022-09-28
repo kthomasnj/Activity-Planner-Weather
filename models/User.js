f19eb00f07d10e7588d02c1c8a2d9b34301f32a5
devin <devin@devins-MacBook-Pro.local>
2022-09-27 21:17:00 -0400

    added hooks to User model

--- models/User.js
+++ models/User.js
@@ -41,5 +41,22 @@ class User extends Model {
         },
       },
     },
+    {
+        hooks: {
+          beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
   );
 module.exports = User;
