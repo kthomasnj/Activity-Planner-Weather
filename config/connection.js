e7c417aaf7fb0d5edea4fed09c95ad06757e0d42
hasnafar <hasnafarrukh@gmail.com>
2022-09-29 22:03:36 -0400

    Fix connection file

--- config/connection.js
+++ config/connection.js
@@ -1,39 +1,15 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
-
-//Cloud DB connection
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_URL,
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
-//localHost DB connection
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
